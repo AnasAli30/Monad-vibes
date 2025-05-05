8d4b377ba5fec49140078b22be02c893737c6470
ShadowX <133592141+AnasAli30@users.noreply.github.com>
2025-05-05 11:22:26 +0530

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,14 +3,13 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    accountAssociation: {
-      header:
-        "",
-      payload:
-        "",
-      signature:
-        "",
-    },
+
+  "accountAssociation": {
+    "header": "eyJmaWQiOjI0OTcwMiwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweEVCNDRFYTBlODBhQzE4MjIwREM5RjY0MjEyRWI3OTAwMzAwMTAxNjUifQ",
+    "payload": "eyJkb21haW4iOiJtb25hZC12aWJlcy1zZGYudmVyY2VsLmFwcCJ9",
+    "signature": "MHhmYTczY2M3NWI2NTA3YjJmNWUzYmQ3N2M0MDg3MjY4YmQwYjQ1MzUwN2QyNmJmNzdmNWUyZjEwNDA3OTFhN2E5MmZmOGYwNTdiNGE2YWE4ZDE2NmEwNjgwNDc4ZTc1NWVjNzUxYTdjYmVjNDNjNDI5MjFkNWNlNDYzMzFlNmI1YzFj"
+  },
+
     frame: {
       version: "1",
       name: "Monad Vibe",
